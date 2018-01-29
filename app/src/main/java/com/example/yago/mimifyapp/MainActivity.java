13152270c9f9198e5f97ebab521c457a3e986ae3
yago <yagofvv@gmail.com>
2018-01-29 12:26:43 +0100

    Beta version. Added new activity and image button.

--- app/src/main/java/com/example/yago/mimifyapp/MainActivity.java
+++ app/src/main/java/com/example/yago/mimifyapp/MainActivity.java
@@ -4,47 +4,40 @@ import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends Activity {
 
     EditText input;
-    EditText output;
-    Button botonMimify;
-    Button botonCopy;
+    ImageButton botonMimify;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        input = (EditText)findViewById(R.id.inputText);
-        output = (EditText)findViewById(R.id.outputText);
-        botonMimify = (Button)findViewById(R.id.button_mimify);
-        botonCopy = (Button)findViewById(R.id.button_clipboard);
+        input = findViewById(R.id.inputText);
+        botonMimify = findViewById(R.id.button_mimify);
 
         botonMimify.setOnClickListener(
                 new View.OnClickListener()
                 {
                     public void onClick(View view)
                     {
+
+
                         mimify();
+
                     }
                 }
         );
-
-        botonCopy.setOnClickListener(
-                new View.OnClickListener()
-                {
-                    public void onClick(View view)
-                    {
-                        copyToClipboard();
-                    }
-                }
-        );
-
 
 
     }
@@ -54,13 +47,12 @@ public class MainActivity extends Activity {
         String mimified = toMimify.replace('e','i').replace('a','i').replace('o','i').replace('u','i').replace('A','I').
                 replace('E','I').replace('O','I').replace('U','I').replace('é','í').replace('á','í').
                 replace('ó','í').replace('ú','í').replace('Á','Í').replace('É','Í').replace('Ó','Í').replace('Ú','Í');
-        output.setText(mimified);
+        Intent intent = new Intent(this, MimifiedActivity.class);
+
+        intent.putExtra("EXTRA_MESSAGE", mimified);
+        startActivity(intent);
     }
-    public void copyToClipboard(){
-        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-        ClipData clip = ClipData.newPlainText("mimified",output.getText());
-        clipboard.setPrimaryClip(clip);
-    }
+
     public void getFromClipboard(){
         ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
         input.setText(clipboard.getText());
@@ -69,8 +61,6 @@ public class MainActivity extends Activity {
     public void onResume(){
         super.onResume();
         ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-        if(output.getText()!=clipboard.getText()) {
             getFromClipboard();
-        }
     }
 }
---
+++ app/src/main/java/com/example/yago/mimifyapp/MimifiedActivity.java
@@ -0,0 +1,49 @@
+package com.example.yago.mimifyapp;
+
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MimifiedActivity extends Activity {
+    TextView mimified;
+    Button copy;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mimified);
+        mimified = findViewById(R.id.mimified);
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra("EXTRA_MESSAGE");
+
+        // Capture the layout's TextView and set the string as its text
+
+        mimified.setText(message);
+
+        copy = findViewById(R.id.copyToClipboard);
+        copy.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+
+
+                        copyToClipboard();
+
+                    }
+                }
+        );
+    }
+    public void copyToClipboard(){
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("mimified",mimified.getText());
+        clipboard.setPrimaryClip(clip);
+    }
+
+}
